16bd25c797d3e4e3bad62effee50e13e38a3b21a
AWRamadhan <annaswahyuramadhan01@gmail.com>
2019-09-07 17:48:56 +0700

    fix error package

--- lelang/src/App.js
+++ lelang/src/App.js
@@ -1,32 +1,11 @@
 import React from "react";
-<<<<<<< HEAD
-import logo from "./logo.svg";
-import "./App.css";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Wira lagi nge test
-        </a>
-      </header>
     </div>
   );
-=======
 
-function App() {
-  return <div className="App"></div>;
->>>>>>> mine
-}
 
 export default App;
--- lelang/src/index.js
+++ lelang/src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 import Login from "./interface/Login";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
