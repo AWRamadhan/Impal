ff6e5720fcd5aeea822e5de67002bd9b43707d84
WiranataK <wiranatak99@gmail.com>
2019-09-07 13:29:16 +0800

    Testing Doang

--- lelang/src/App.js
+++ lelang/src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          gilang adhi
+          Wira lagi nge test
         </a>
       </header>
     </div>
