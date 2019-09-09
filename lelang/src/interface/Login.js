5bd17551c1da2840a5f8704561bc974ebb1623a7
WiranataK <wiranatak99@gmail.com>
2019-09-09 11:34:06 +0800

    Add Container and Jumbotron
    to make a best looking web

--- lelang/src/interface/Login.js
+++ lelang/src/interface/Login.js
@@ -1,16 +1,19 @@
 import React, { Component } from "react";
 import Header from "./Header";
 import Navbars from "./Navbars";
+import { Jumbotron } from "react-bootstrap";
 class Login_Forms extends Component {
   render() {
     return (
       <div className="Login">
         <Navbars />
-        <Header />
-        <form className="text-center">
-          <div className="form-group"></div>
-          <button type="submit">Masuk</button>
-        </form>
+        <Jumbotron>
+          <Header />
+          <form className="text-center">
+            <div className="form-group"></div>
+            <button type="submit">Masuk</button>
+          </form>
+        </Jumbotron>
       </div>
     );
   }
--- lelang/src/interface/Navbars.js
+++ lelang/src/interface/Navbars.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Navbar } from 'react-bootstrap';
+import { Navbar, Container } from 'react-bootstrap';
 import { Nav } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
 import { Form } from 'react-bootstrap';
@@ -9,24 +9,26 @@ class Navbars extends Component {
     return (
       <div className="navbars">
         <Navbar bg="dark" variant="dark">
-          <Navbar.Brand href="#home">
-            <img
-              src="https://thumbs.dreamstime.com/b/auction-vector-stamp-isolated-white-background-84941491.jpg"
-              width="40"
-              height="40"
-              className="d-inline-block align-top"
-              alt="AuctioN"
-            />
-          </Navbar.Brand>
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-          <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-info">Search</Button>
-          </Form>
+          <Container>
+            <Navbar.Brand href="#home">
+              <img
+                src="https://thumbs.dreamstime.com/b/auction-vector-stamp-isolated-white-background-84941491.jpg"
+                width="40"
+                height="40"
+                className="d-inline-block align-top"
+                alt="AuctioN"
+              />
+            </Navbar.Brand>
+            <Nav className="mr-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+            <Form inline>
+              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+              <Button variant="outline-info">Search</Button>
+            </Form>
+          </Container>
         </Navbar>
       </div>
     );
