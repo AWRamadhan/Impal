252b93ef8e190616304765b4a0ed7fec43639d1d
viosagara347 <viosagara347@gmail.com>
2019-12-03 08:27:04 +0700

    Update contactform.js

--- Fresh Start/Lelang/assets/contactform/contactform.js
+++ Fresh Start/Lelang/assets/contactform/contactform.js
@@ -100,6 +100,7 @@ jQuery(document).ready(function($) {
       data: str,
       success: function(msg) {
         // alert(msg);
+        if (msg == 'OK') {
           $("#sendmessage").addClass("show");
           $("#errormessage").removeClass("show");
           $('.contactForm').find("input, textarea").val("");
